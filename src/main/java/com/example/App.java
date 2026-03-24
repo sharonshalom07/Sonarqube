69d4c1c9772229c0362df73933863f1ab59e185e
SHALOM SHARON MOSES <5023152@it.fcrit.ac.in>
2026-03-24 14:18:55 +0530

    Sonarqube

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main application entry point.
+ */
+public class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    /**
+     * Main method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
